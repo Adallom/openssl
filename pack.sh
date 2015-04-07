0071ca226a12fadeee918ece4fbb46c19acd6a96
root <root@b0acab151496.(none)>
2015-04-07 12:13:39 +0000

    Fixed script to work with nginx build

--- pack.sh
+++ pack.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-./config --prefix=$PWD/openssl-build no-shared
+./config --prefix=$PWD/openssl-build/.openssl no-shared
 make
 make install
+
+# HACK: These dummy files are required for the nginx --with-openssl configure script
+echo -ne "#!/bin/bash\necho Dummy\n" > $PWD/openssl-build/config
+chmod +x $PWD/openssl-build/config
+echo -ne "install:\n\techo Dummy\nclean:\n\techo Dummy\n" > $PWD/openssl-build/Makefile
+
 fpm -t deb -s dir --name openssl-build \
     -v 1.0.2a \
     --vendor "Adallom" \
