d1cc61346e44b9f9de66eb76122f0e17ce3dfa54
Aviram Cohen <aviram@adallom.com>
2016-04-17 13:39:18 +0300

    Added the install_sw target to the fake Makefile

--- pack.sh
+++ pack.sh
@@ -6,7 +6,7 @@ make install
 # HACK: These dummy files are required for the nginx --with-openssl configure script
 echo -ne "#!/bin/bash\necho > /dev/null\n" > $PWD/openssl-build/config
 chmod +x $PWD/openssl-build/config
-echo -ne "install:\n\techo > /dev/null\nclean:\n\techo > /dev/null\n" > $PWD/openssl-build/Makefile
+echo -ne "install_sw:\n\techo > /dev/null\ninstall:\n\techo > /dev/null\nclean:\n\techo > /dev/null\n" > $PWD/openssl-build/Makefile
 
 fpm -t deb -s dir --name openssl-build \
     -v 1.0.2g-1 \
