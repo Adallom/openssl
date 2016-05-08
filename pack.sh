04b1169661ce29172a3e118ce42245f4d20dcf1c
Aviram Cohen <aviram@adallom.com>
2016-05-08 12:28:09 +0300

    Update pack.sh for release 1.0.2h

--- pack.sh
+++ pack.sh
@@ -9,7 +9,7 @@ chmod +x $PWD/openssl-build/config
 echo -ne "install_sw:\n\techo > /dev/null\ninstall:\n\techo > /dev/null\nclean:\n\techo > /dev/null\n" > $PWD/openssl-build/Makefile
 
 fpm -t deb -s dir --name openssl-build \
-    -v 1.0.2g-1 \
+    -v 1.0.2h-1 \
     --vendor "Adallom" \
     --url "https://github.com/Adallom/openssl" \
     --prefix "/opt" \
